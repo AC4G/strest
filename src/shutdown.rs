1ece6ae250148f7c9f99e64f7f1e72922e67d2b6
AC4G <vost_400@gmx.de>
2025-07-23 23:06:28 +0200

    Simplify shutdown handler by removing tokio task around the main blocking task

--- src/shutdown.rs
+++ src/shutdown.rs
@@ -5,26 +5,19 @@ use tokio::sync::broadcast;
 
 pub fn setup_shutdown_handler(shutdown_tx: &broadcast::Sender<u16>) -> tokio::task::JoinHandle<()> {
     let shutdown_tx = shutdown_tx.clone();
-    let mut shutdown_rx = shutdown_tx.subscribe();
 
-    tokio::spawn(async move {
-        tokio::select! {
-            _ = tokio::task::spawn_blocking(move || {
-                loop {
-                    if let Ok(Event::Key(KeyEvent {
-                        code: KeyCode::Char('c'),
-                        modifiers: KeyModifiers::CONTROL,
-                        ..
-                    })) = read()
-                    {
-                        let _ = shutdown_tx.send(1);
-                        break;
-                    }
-                    std::thread::sleep(Duration::from_millis(100));
-                }
-            }) => {},
-
-            _ = shutdown_rx.recv() => return
+    tokio::task::spawn_blocking(move || {
+        loop {
+            if let Ok(Event::Key(KeyEvent {
+                code: KeyCode::Char('c'),
+                modifiers: KeyModifiers::CONTROL,
+                ..
+            })) = read()
+            {
+                let _ = shutdown_tx.send(1);
+                break;
+            }
+            std::thread::sleep(Duration::from_millis(100));
         }
     })
 }
